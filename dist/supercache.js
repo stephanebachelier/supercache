75cb963f3c195808df81a95a0ac02291dd6cdbfa
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-12-13 02:36:26 +0100

    Release 0.0.1

---
+++ dist/supercache.js
@@ -0,0 +1,60 @@
+/*! supercache - v0.0.1
+ *  Release on: 2014-12-13
+ *  Copyright (c) 2014 Stéphane Bachelier
+ *  Licensed MIT */
+(function (root, factory) {
+  'use strict';
+  if (typeof define === 'function' && define.amd) {
+    define(function () {
+      return factory();
+    });
+  }
+  else if (typeof exports !== 'undefined') {
+    module.exports = factory();
+  }
+  else {
+    root.supercache = factory();
+  }
+
+}(this, function () {
+  'use strict';
+
+  var cache = Object.create(null);
+
+  var cacheIt = function (res) {
+    cache[res.req.url] = {
+      content: res.text,
+      parse: res.req.parse[res.req.type]
+    };
+  };
+
+  var readFromCache = function (url) {
+    var cachedContent = cache[url];
+
+    if (!cachedContent) {
+      return null;
+    }
+
+    // return deserialize response
+    return cachedContent.content && cachedContent.parse ? cachedContent.parse(cachedContent.text) : null;
+  };
+
+  var clearCache = function () {
+    cache = null;
+  };
+
+  var writeToCache = function (req) {
+    if (!req) {
+      return;
+    }
+
+    // listen to request
+    req.on('response', cacheIt);
+  };
+
+  return {
+    writeToCache: writeToCache,
+    readFromCache: readFromCache,
+    clearCache: clearCache
+  };
+}));
